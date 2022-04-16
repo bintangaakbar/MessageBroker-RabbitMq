60d642c2a73c6ce936a3fd7c38472c44afbf6708
bintangaakbar <Banugrah2@gmail.com>
2022-04-16 12:18:35 +0700

    EAI DONE

--- consumer.py
+++ consumer.py
@@ -1,19 +1,20 @@
-#!/usr/bin/env python
-import pika, sys, os
+import pika #menggunakan library pika
+import sys  #menggunakan library system
+import os   #menggunakan libray Operating System
 
 def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-    channel = connection.channel()
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))   #parameter menggunakan localhost
+    channel = connection.channel()  #memulai sebuah channel
 
-    channel.queue_declare(queue='EAI')
+    channel.queue_declare(queue='EAI') #mendeclare sebuah queue
 
-    def callback(ch, method, properties, body):
+    def callback(ch, method, properties, body): #code ini akan mencetak sebuah pesan yang dikirimkan dari producer
         print(" [x] Received %r" % body)
 
-    channel.basic_consume(queue='EAI', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue='EAI', on_message_callback=callback, auto_ack=True) #code ini akan menerima pesan yang dikirimkan dari producer dengan queue EAI
 
     print('Sedang menunggu pesan, klik CTRL + C untuk keluar!')
-    channel.start_consuming()
+    channel.start_consuming()   #memulai untuk menerima pesan dari queue EAI yang dikirimkan oleh producer
 
 if __name__ == '__main__':
     try:
--- producer.py
+++ producer.py
@@ -1,13 +1,11 @@
-#!/usr/bin/env python
-import pika
+import pika  #menggunakan library pika
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.queue_declare(queue='EAI')
+channel.queue_declare(queue='EAI')  #mendeklarasikan queue di rabbitmq menggunakan nama EAI
 
-body = input("Masukkan Pesan anda: ")
-channel.basic_publish(exchange='', routing_key='EAI', body=body)
-print(" [x] Mengirimkan", body)
-connection.close()
+body = input("Masukkan Pesan anda: ")   #inputan user untuk mengirimkan pesan
+channel.basic_publish(exchange='', routing_key='EAI', body=body)    #mengirimkan pesan menggunakan queue EAI dengan isi pesan dari inputan user
+print(" [x] Mengirimkan", body) #bukti kalau code sudah berjalan
+connection.close() #koneksi code dengan rabbitmq langsung di tutup setelah pesan terkirim
